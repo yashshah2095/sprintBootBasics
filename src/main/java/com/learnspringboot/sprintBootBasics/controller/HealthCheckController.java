4e058b1679283341a643c42fccc197d8911f312e
Yash <yashshah2095@gmail.com>
2025-08-27 17:00:01 +0530

    some more changes

--- src/main/java/com/learnspringboot/sprintBootBasics/controller/HealthCheckController.java
+++ src/main/java/com/learnspringboot/sprintBootBasics/controller/HealthCheckController.java
@@ -1,12 +1,15 @@
 package com.learnspringboot.sprintBootBasics.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckController {
+    @Value ("${owner.name}")
+    private String ownerName;
     @GetMapping("health-check")
     public String getHealthCheck(){
-        return "OK";
+        return "OK " + ownerName ;
     }
 }
